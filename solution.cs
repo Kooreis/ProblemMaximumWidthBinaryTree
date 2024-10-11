58d21a8c40493003499210edca469a4f244f5e6c
Kooreis <kooreis.official@gmail.com>
2024-10-11 06:53:46 -0500

    This part of the script defines the Node class, which is used to create nodes for the binary tree. Each node contains an integer data and references to its left and right child nodes.

--- solution.cs
+++ solution.cs
@@ -1,4 +1,3 @@
-```C#
 using System;
 using System.Collections.Generic;
 
@@ -13,66 +12,3 @@ public class Node
         left = right = null;
     }
 }
-
-public class BinaryTree
-{
-    Node root;
-
-    public int getMaxWidth(Node node)
-    {
-        int maxWidth = 0;
-        int width;
-        int h = height(node);
-        int i;
-
-        for (i = 1; i <= h; i++)
-        {
-            width = getWidth(node, i);
-            if (width > maxWidth)
-                maxWidth = width;
-        }
-
-        return maxWidth;
-    }
-
-    int getWidth(Node node, int level)
-    {
-        if (node == null)
-            return 0;
-
-        if (level == 1)
-            return 1;
-        else if (level > 1)
-            return getWidth(node.left, level - 1) + getWidth(node.right, level - 1);
-        return 0;
-    }
-
-    int height(Node node)
-    {
-        if (node == null)
-            return 0;
-        else
-        {
-            int lHeight = height(node.left);
-            int rHeight = height(node.right);
-
-            return (lHeight > rHeight) ? (lHeight + 1) : (rHeight + 1);
-        }
-    }
-
-    public static void Main(string[] args)
-    {
-        BinaryTree tree = new BinaryTree();
-        tree.root = new Node(1);
-        tree.root.left = new Node(2);
-        tree.root.right = new Node(3);
-        tree.root.left.left = new Node(4);
-        tree.root.left.right = new Node(5);
-        tree.root.right.right = new Node(8);
-        tree.root.right.right.left = new Node(6);
-        tree.root.right.right.right = new Node(7);
-
-        Console.WriteLine("Maximum width is " + tree.getMaxWidth(tree.root));
-    }
-}
-```
