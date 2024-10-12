8121efe02de8ffb5517bbc4fd8d9ff8a2568fbc5
Kooreis <kooreis.official@gmail.com>
2024-10-12 05:53:46 -0500

    This update introduces the max_width function which calculates the maximum width of the binary tree. It starts by checking if the root is None, in which case it returns 0. It then initializes the max_width variable and a queue, and adds the root to the queue.

--- solution.py
+++ solution.py
@@ -1,5 +1,7 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.left = None
-        self.right = None
+def max_width(root):
+    if root is None:
+        return 0
+
+    max_width = 0
+    queue = []
+    queue.append(root)
